0359765789c5256ccef16e85dec3c68487c0056f
rewhsu <andrewhsu42@gmail.com>
2017-01-07 11:52:33 -0800

    Pass all tests up to VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer video={{}}/>
+      <VideoPlayer video={window.exampleVideoData[0]} />
     </div>
     <div className="col-md-5">
       <VideoList videos={window.exampleVideoData} />
